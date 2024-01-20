ceb6fb6c5a0d523102860ed89b5e3470437d2248
pradeep.mudvari@dimitra.io <Pradeep>
2024-01-20 09:55:13 +0545

    add chatbox and message

---
+++ src/components/ChatBox.vue
@@ -0,0 +1,51 @@
+<template>
+  <form class='chat-box' @submit='onSubmit($event)'>
+    <input
+        v-model='text'
+        placeholder='Write a message..'
+        type='text'
+    />
+    <button :disabled='text === ""'>Send</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'ChatBox',
+  data() {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    onSubmit(event) {
+      this.$emit("submit", event, this.text);
+      this.text = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+.chat-box {
+  width: 100%;
+  display: flex;
+}
+
+input {
+  width: 80%;
+  height: 40px;
+  margin-right: 10px;
+}
+
+button:disabled {
+  opacity: 0.5;
+}
+
+button {
+    width: 20%;
+    border: 1px solid skyblue;
+    background: skyblue;
+    color: #fff;
+}
+</style>
--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,33 +1,58 @@
 <template>
 <Header/>
     <h1>Users</h1>
-    <div class="users">
-        <tr>
-            <th> Name </th>
-            <th> Email </th>
-            <th> Action</th>
-        </tr>
-        <tr v-for ="item in users" :key="item.id">
-            <td>{{item.name}}</td>
-            <td>{{item.email}}</td>
-            <td>
-                <button>Message</button>
-            </td>
-        </tr>
+    <div class="display">
+        <div class="users">
+            <tr>
+                <th> Name </th>
+                <th> Email </th>
+                <th> Action</th>
+            </tr>
+            <tr v-for ="item in users" :key="item.id">
+                <td>{{item.name}}</td>
+                <td>{{item.email}}</td>
+                <td>
+                    <button @click="showChat(item.uid)">Message</button>
+                </td>
+            </tr>
+        </div>
+        <div class='messages' v-if="showPopUp === true">
+            <Message
+                    v-for='message in messages'
+                    :key='message.id'
+                    :class='["message", { right: message.isMine }]'
+                    :dark='message.isMine'
+                    :text='message.text'
+                    :author='message.author'
+            />
+            <ChatBox
+                class='chat-box'
+                @submit='onSubmit'
+            />
+        </div>
+        <div v-else/>
     </div>
+
 </template>
 
 <script>
+import ChatBox from './ChatBox.vue';
 import Header from './Header.vue';
+import Message from './Message.vue';
 
 export default {
     name: "Users",
     components: {
         Header,
+        ChatBox,
+        Message
     },
     data() {
         return {
             showPopUp: false,
+            user: {
+                id: 1
+            },
             users: [
                 {
                 id: 1,
@@ -45,12 +70,69 @@ export default {
                 name: 'User 3',
                 }
             ],
+            messages: []
+        }
+    },
+    methods: {
+        showChat(userId) {
+            console.log(userId);
+            this.getChat();
+            this.showPopUp = true;
+        },
+        getChat() {
+            // listenChat((chat) => {//fetch messages from backend
+            const chat = [
+                {
+                id:1,
+                text: 'hi',
+                uid: 1,
+                author: 'User 1'
+                },
+                {
+                id:2,
+                text: 'hi',
+                uid: 2,
+                author: 'User 2'
+                },
+                {
+                id:3,
+                text: 'how are you',
+                uid: 1,
+                author: 'User 1'
+                },
+                {
+                id:4,
+                text: 'I m good what abt u??',
+                uid: 2,
+                author: 'User 2'
+                },
+            ];
+                this.messages = chat.map(m => ({
+                ...m,
+                isMine: m.uid && m.uid === this.user?.id
+                }));
+            // });
+        },
+        onSubmit(event, text) {
+            event.preventDefault();
+            
+            console.log('Message sent', text);
+            // sendMessage({//send messages to backend
+            //     text,
+            //     uid: this.user?.id,
+            //     author: this.user?.name
+            // });
+            // this.getChat();
         }
     }
 }
 </script>
 
-<style>
+<style scoped>
+.display {
+    display: flex;
+}
+
 .users tr{
     width: 80px;
     padding: 10px;
@@ -60,10 +142,28 @@ export default {
     padding: 10px;
 }
 .users button{
-        width: 80px;
-        height: 40px;
-        border: 1px solid skyblue;
-        background: skyblue;
-        color: #fff;
+    width: 80px;
+    height: 40px;
+    border: 1px solid skyblue;
+    background: skyblue;
+    color: #fff;
 }
+
+.messages {
+  flex-grow: 1;
+  overflow: auto;
+  padding: 1rem;
+  border: 1px solid skyblue;
+  margin: 10px;
+  max-width: 30%;
+}
+
+.message {
+  margin: 0.5rem 0rem;
+}
+
+.message.right {
+  margin-left: auto;
+}
+
 </style>
